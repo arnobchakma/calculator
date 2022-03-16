c803577db6d0ef6b3c98da67092075332c39e0b3
arnobchakma <arnobchakma05@gmail.com>
2022-03-16 15:20:45 -0600

    deploy project

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALP_btbyjJlyFaFUdDdLOSrE1JWO_V56U",
+    authDomain: "calculator-6d573.firebaseapp.com",
+    projectId: "calculator-6d573",
+    storageBucket: "calculator-6d573.appspot.com",
+    messagingSenderId: "789050473681",
+    appId: "1:789050473681:web:29490b921b7a2247e599f0"
+  };
+
+export default firebaseConfig;
